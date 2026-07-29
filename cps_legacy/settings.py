e33d6adaa51b4fee24cd3477e98b7e091d54a122
Yusuf Muhammad Adam <yusufkolamari@gmial.com>
2026-07-29 14:36:27 +0100

    Update storage configuration for Cloudinary

--- cps_legacy/settings.py
+++ cps_legacy/settings.py
@@ -98,10 +98,28 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # Cloudinary Media Storage Settings
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+# CLOUDINARY_STORAGE = {
+#     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+#     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+#     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+# }
+
+# MEDIA_URL = '/media/'
+
+# Cloudinary Configuration
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
 }
 
 MEDIA_URL = '/media/'
